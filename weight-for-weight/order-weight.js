2bb6145b6e168770ff87cc4fe3596f3c5b698bc0
AlekseyLeshko <alekseyleshko@gmail.com>
2018-03-25 17:01:17 +0400

    Add weight-for-weight task

---
+++ weight-for-weight/order-weight.js
@@ -0,0 +1,15 @@
+export default string =>
+  string
+    .split(' ')
+    .map(num => ({
+      val: num,
+      weight: num.split('').reduce((weight, char) => (weight += +char), 0),
+    }))
+    .sort(
+      (a, b) =>
+        a.weight === b.weight
+          ? a.val.localeCompare(b.val)
+          : a.weight - b.weight,
+    )
+    .map(item => item.val)
+    .join(' ');
