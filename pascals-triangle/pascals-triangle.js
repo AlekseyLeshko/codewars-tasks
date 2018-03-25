7229b99357af060064bc3e49ac713bd1845e52e5
AlekseyLeshko <alekseyleshko@gmail.com>
2018-03-25 20:07:57 +0400

    Update decision

--- pascals-triangle/pascals-triangle.js
+++ pascals-triangle/pascals-triangle.js
@@ -1,12 +1,13 @@
 export default n => {
   const list = [[1]];
   for (let i = 1; i < n; i++) {
-    const row = [1];
-    for (let j = 1; j < i; j++) {
-      const val = list[i - 1][j - 1] + list[i - 1][j];
-      row.push(val);
-    }
-    row.push(1);
+    const row = [
+      1,
+      ...[...Array(i - 1)].map(
+        (num, index) => list[i - 1][index] + list[i - 1][index + 1],
+      ),
+      1,
+    ];
     list.push(row);
   }
 
