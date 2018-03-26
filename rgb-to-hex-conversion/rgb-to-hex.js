869b685e8a845f17369dc72b7329ef4080d89ee9
AlekseyLeshko <alekseyleshko@gmail.com>
2018-03-26 15:38:00 +0400

    Add first decision for task

--- rgb-to-hex-conversion/rgb-to-hex.js
+++ rgb-to-hex-conversion/rgb-to-hex.js
@@ -1,3 +1,12 @@
-export default () => {
-  return 'FFFFFF';
+export default (r, g, b) => {
+  return numToHex(r) + numToHex(g) + numToHex(b);
+};
+
+const numToHex = (num) => {
+  num = num >= 255 ? 255 : num;
+  const str = num.toString(16);
+
+  const hex = str.toUpperCase();
+
+  return hex.length === 1 ? hex + hex : hex;
 };
