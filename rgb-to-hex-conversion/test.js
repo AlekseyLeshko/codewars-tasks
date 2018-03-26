56dfcd0abdfbe412562aa024e6cad0aa5fac16c4
AlekseyLeshko <alekseyleshko@gmail.com>
2018-03-26 15:22:21 +0400

    Add rgb-to-hex-conversion task

---
+++ rgb-to-hex-conversion/test.js
@@ -0,0 +1,11 @@
+import { describe } from 'ava-spec';
+import rgbToHex from './rgb-to-hex';
+
+describe('rgb to hex conversion:', it => {
+  it('should return hex', t => {
+    t.is(rgbToHex(255, 255, 255), 'FFFFFF');
+    t.is(rgbToHex(255, 255, 300), 'FFFFFF');
+    t.is(rgbToHex(0, 0, 0), '000000');
+    t.is(rgbToHex(148, 0, 211), '9400D3');
+  });
+});
