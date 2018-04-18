99c4674c24612f6e6a80fd84c722e76e84852f44
AlekseyLeshko <alekseyleshko@gmail.com>
2018-04-18 13:16:28 +0400

    Add decision

--- strip-url-params/strip-url-params.js
+++ strip-url-params/strip-url-params.js
@@ -1,1 +1,27 @@
-export default () => '';
+export default (baseUrl, ignoreParams = []) => {
+  const [domain, ...queryArr] = baseUrl.split(/\?|&|=/g);
+  const paramArr = queryArr.reduce((arr, item, index) => {
+    if (
+      index % 2 === 0 &&
+      !arr.includes(item) &&
+      !ignoreParams.includes(item)
+    ) {
+      arr.push(item);
+      arr.push(queryArr[index + 1]);
+    }
+
+    return arr;
+  }, []);
+
+  const queryStr = paramArr.reduce(
+    (str, item, index) =>
+      index % 2 === 0
+        ? (str += `${item}=`)
+        : index !== paramArr.length - 1
+          ? (str += `${item}&`)
+          : (str += item),
+    '',
+  );
+
+  return `${domain}${queryStr.length === 0 ? '' : `?${queryStr}`}`;
+};
