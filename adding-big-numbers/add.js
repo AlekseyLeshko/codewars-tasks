90ab0af086fe49f46da36ca4a7dacd24ab80e9f8
AlekseyLeshko <alekseyleshko@gmail.com>
2018-04-19 17:07:20 +0400

    Add decision for task

--- adding-big-numbers/add.js
+++ adding-big-numbers/add.js
@@ -1,1 +1,30 @@
-export default () => '';
+export default (x, y) => {
+  let a = x.split('').reverse().map(Number);
+  let b = y.split('').reverse().map(Number);
+
+  let length;
+  let bigArr;
+  let secondArr;
+
+  if (a.length > b.length) {
+    length = a.length;
+    bigArr = a.concat(Array(length - a.length + 0).fill(0));
+    secondArr = b;
+  } else {
+    length = b.length;
+    bigArr = b.concat(Array(length - b.length + 1).fill(0));
+    secondArr = a;
+  }
+
+  for (let i = 0; i < length; i++) {
+    bigArr[i] += secondArr[i];
+    bigArr[i + 1] += Math.floor(bigArr[i] / 10);
+    bigArr[i] %= 10;
+  }
+
+  if (bigArr[length] === 0) {
+    bigArr.splice(-1, 1);
+  }
+
+  return bigArr.reverse().join('');
+};
--- adding-big-numbers/test.js
+++ adding-big-numbers/test.js
@@ -3,6 +3,8 @@ import {describe} from 'ava-spec';
 
 describe('adding big numbers:', it => {
   it('should return amount', t => {
+    t.deepEqual(add('1', '9'), '10');
     t.deepEqual(add('123', '321'), '444');
+    t.deepEqual(add('11', '99'), '110');
   });
 });
