a1087394bd0365297c690adf0b41d3a32e58f9df
AlekseyLeshko <alekseyleshko@gmail.com>
2018-04-21 01:17:14 +0400

    Add decision for task

--- roman-numerals-decoder/solution.js
+++ roman-numerals-decoder/solution.js
@@ -1,1 +1,20 @@
-export default () => 0;
+const romans = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+export default roman =>
+  roman
+    .split('')
+    .reduce(
+      (sum, n, index, arr) =>
+        romans[n] < romans[arr[index + 1]]
+          ? (sum -= romans[n])
+          : (sum += romans[n]),
+      0,
+    );
