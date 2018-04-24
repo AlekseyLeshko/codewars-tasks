75e326c79814c52cf6a23d7c76ae92fbea4566c5
AlekseyLeshko <alekseyleshko@gmail.com>
2018-04-25 00:01:08 +0400

    Add decison for task

--- point-in-polygon-1/point-in-poly.js
+++ point-in-polygon-1/point-in-poly.js
@@ -1,1 +1,16 @@
-export default () => true;
+export default (arr, point) => {
+  const xp = arr.map(p => p[0]);
+  const yp = arr.map(p => p[1]);
+  const [x, y] = point;
+  let c = false;
+
+  for (let i = 0, j = arr.length - 1; i < arr.length; j = i++) {
+    if (
+      ((yp[i] <= y && y < yp[j]) || (yp[j] <= y && y < yp[i])) &&
+      x > (xp[j] - xp[i]) * (y - yp[i]) / (yp[j] - yp[i]) + xp[i]
+    )
+      c = !c;
+  }
+
+  return c;
+};
