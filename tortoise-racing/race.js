f0ae1e6d370cc09cb62963d08372a7b9c0ce18d6
AlekseyLeshko <alekseyleshko@gmail.com>
2018-05-04 11:00:21 +0400

    Add decision for task

--- tortoise-racing/race.js
+++ tortoise-racing/race.js
@@ -1,1 +1,8 @@
-export default () => [];
+export default (v1, v2, g) =>
+  v1 >= v2
+    ? null
+    : new Date(Math.round(3600000 * (g / Math.floor(v2 - v1))))
+      .toISOString()
+      .substr(11, 8)
+      .split(':')
+      .map(Number);
--- tortoise-racing/test.js
+++ tortoise-racing/test.js
@@ -3,13 +3,13 @@ import race from './race';
 
 describe('tortoise racing:', it => {
   it('should return needed time in hours, minutes and second', t => {
-    t.is(race(720, 850, 70), [0, 32, 18]);
-    t.is(race(80, 91, 37), [3, 21, 49]);
-    t.is(race(80, 100, 40), [2, 0, 0]);
-    t.is(race(720, 850, 37), [0, 17, 4]);
-    t.is(race(720, 850, 370), [2, 50, 46]);
-    t.is(race(120, 850, 37), [0, 3, 2]);
-    t.is(race(820, 850, 550), [18, 20, 0]);
-    t.is(race(820, 81, 550), null);
+    t.deepEqual(race(720, 850, 70), [0, 32, 18]);
+    t.deepEqual(race(80, 91, 37), [3, 21, 49]);
+    t.deepEqual(race(80, 100, 40), [2, 0, 0]);
+    t.deepEqual(race(720, 850, 37), [0, 17, 4]);
+    t.deepEqual(race(720, 850, 370), [2, 50, 46]);
+    t.deepEqual(race(120, 850, 37), [0, 3, 2]);
+    t.deepEqual(race(820, 850, 550), [18, 20, 0]);
+    t.deepEqual(race(820, 81, 550), null);
   });
 });
