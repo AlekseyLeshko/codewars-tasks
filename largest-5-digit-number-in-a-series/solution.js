baed72763af0a547028cf5f4bd70900096447d72
AlekseyLeshko <alekseyleshko@gmail.com>
2018-05-04 11:11:01 +0400

    Add decison for task

--- largest-5-digit-number-in-a-series/solution.js
+++ largest-5-digit-number-in-a-series/solution.js
@@ -1,1 +1,16 @@
-export default () => 0;
+export default digits =>
+  digits
+    .split('')
+    .map(Number)
+    .reduce((arr, n, index, baseArr) => {
+      if (index <= baseArr.length - 5) {
+        const substr = baseArr.slice(index, index + 5).join('');
+
+        arr.push(substr);
+      }
+
+      return arr;
+    }, [])
+    .map(Number)
+    .sort()
+    .pop();
