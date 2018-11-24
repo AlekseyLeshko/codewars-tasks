9e0bdea73d96faccf66397db774740f89719d3b6
alekseyleshko <alekseyleshko@gmail.com>
2018-11-25 01:43:45 +0300

    Add simple decision

--- simple-events/event.js
+++ simple-events/event.js
@@ -1,4 +1,21 @@
 class Event {
+  constructor() {
+    this.handlers = {};
+  }
+
+  subscribe(fun) {
+    this.handlers[fun] = fun;
+  }
+
+  unsubscribe(fun) {
+    delete this.handlers[fun];
+  }
+
+  emit() {
+    Object
+      .values(this.handlers)
+      .map(fun => fun(...arguments));
+  }
 }
 
 export default Event;
--- simple-events/test.js
+++ simple-events/test.js
@@ -2,8 +2,31 @@ import Event from './event';
 import { describe } from 'ava-spec';
 
 describe('simple events:', it => {
-  it('should work like rules', t => {
+  it('should work with many handlers', t => {
     const event = new Event();
+
     t.pass(event);
+
+    function f() {
+      f.calls = (f.calls || 0) + 1;
+      f.args = Array.prototype.slice.call(arguments);
+    }
+    function f2() {
+      f2.calls = (f2.calls || 0) + 1;
+      f2.args = Array.prototype.slice.call(arguments);
+    }
+
+    event.subscribe(f);
+    event.subscribe(f2);
+    event.emit(1, 'foo', true);
+
+    t.is(f.calls, 1);
+    t.is(f2.calls, 1);
+
+    event.unsubscribe(f);
+    event.emit(2);
+
+    t.is(f.calls, 1);
+    t.is(f2.calls, 2);
   });
 });
