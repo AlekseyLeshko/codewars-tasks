28bf81cc3d7acba8f4f5c2890ced9cb970162c69
alekseyleshko <alekseyleshko@gmail.com>
2018-12-03 01:10:05 +0300

    First implementation

--- haskell-list-dot-notation/list.js
+++ haskell-list-dot-notation/list.js
@@ -1,1 +1,45 @@
-export default () => [];
+const isRange = (str) => str.includes('..');
+
+export default (options = { generator: ''  }) => {
+  let generator = options.generator || '';
+  generator = generator.trim();
+  if (!generator.length) {
+    return [];
+  }
+
+  const blocks = generator.split(',');
+  const list = blocks
+    .reduce((acc, str, index) => {
+      if (isRange(str)) {
+        const arr = str.split('..');
+        const end = Number.parseInt(arr[1], 10);
+        const start = Number.parseInt(arr[0], 10);
+        const a = blocks[index - 1];
+        const step = a ? (start - Number.parseInt(a, 10)) : 0;
+
+        if (end >= start && step === 0) {
+          acc = acc.concat(Array(end - start + 1).fill(start).map((_, index) => start + index));
+          return acc;
+        }
+
+        if (end >= start && step > 0) {
+          const count = end === start ? 1 : Math.ceil((end - start) / step);
+          acc = acc.concat(Array(count).fill(start).map((_, index) => start + (index * step)));
+          return acc;
+        }
+
+        if (end <= start && step < 0) {
+          const count = Math.abs(Math.ceil((end - start) / step)) + 1;
+          acc = acc.concat(Array(count).fill(start).map((_, index) => start + (index * step)));
+          return acc;
+        }
+
+        return acc;
+      }
+
+      acc.push(Number.parseInt(str, 10));
+      return acc;
+    }, []);
+
+  return list;
+};
--- haskell-list-dot-notation/test.js
+++ haskell-list-dot-notation/test.js
@@ -1,8 +1,36 @@
-import List from './list';
+import ArrayComprehension from './list';
 import { describe } from 'ava-spec';
 
 describe('Haskell List Dot Notation:', it => {
-  it('should work like list from haskell', t => {
-    t.deepEqual(List(), []);
+  it('should generate empty list', t => {
+    t.deepEqual(ArrayComprehension(), []);
+    t.deepEqual(ArrayComprehension({}), []);
+    t.deepEqual(ArrayComprehension({ generator: '   ' }), []);
+  });
+
+  it('should generate list with one item', t => {
+    t.deepEqual(ArrayComprehension({ generator: '1' }), [1]);
+  });
+
+  it('should generate list with two items', t => {
+    t.deepEqual(ArrayComprehension({ generator: '1,2' }), [1, 2]);
+    t.deepEqual(ArrayComprehension({ generator: '1 ,   2' }), [1, 2]);
+  });
+
+  it('should generate list with some items', t => {
+    t.deepEqual(ArrayComprehension({ generator: '1,4,6,3,-2' }), [1, 4, 6, 3, -2]);
+  });
+
+  it('should generate list with range', t => {
+    t.deepEqual(ArrayComprehension({generator: '1,3..4'}), [1,3]);
+    t.deepEqual(ArrayComprehension({generator: '1,2..2'}), [1,2]);
+    t.deepEqual(ArrayComprehension({generator: '3,2..2'}), [3,2]);
+    t.deepEqual(ArrayComprehension({generator: '5..3'}), []);
+    t.deepEqual(ArrayComprehension({generator: '90..80'}), []);
+    t.deepEqual(ArrayComprehension({generator: '8,4..40'}), [8]);
+    t.deepEqual(ArrayComprehension({generator: '1,90..80'}), [1]);
+    t.deepEqual(ArrayComprehension({generator: '1..5'}), [1,2,3,4,5]);
+    t.deepEqual(ArrayComprehension({generator: '1,4..12'}), [1,4,7,10]);
+    t.deepEqual(ArrayComprehension({generator: '12,10..4'}), [12,10,8,6,4]);
   });
 });
