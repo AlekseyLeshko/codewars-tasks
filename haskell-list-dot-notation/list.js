da706eb5ac029c390fb6c71b96ecd746305deb48
alekseyleshko <alekseyleshko@gmail.com>
2018-12-03 10:51:35 +0300

    Second disicion

--- haskell-list-dot-notation/list.js
+++ haskell-list-dot-notation/list.js
@@ -7,39 +7,31 @@ export default (options = { generator: ''  }) => {
     return [];
   }
 
-  const blocks = generator.split(',');
-  const list = blocks
-    .reduce((acc, str, index) => {
+  return generator.split(',')
+    .reduce((acc, str, index, sourceArr) => {
       if (isRange(str)) {
         const arr = str.split('..');
+
         const end = Number.parseInt(arr[1], 10);
         const start = Number.parseInt(arr[0], 10);
-        const a = blocks[index - 1];
-        const step = a ? (start - Number.parseInt(a, 10)) : 0;
+        const step = sourceArr[index - 1] ? (start - Number.parseInt(sourceArr[index - 1], 10)) : 1;
+        let count = 0;
 
-        if (end >= start && step === 0) {
-          acc = acc.concat(Array(end - start + 1).fill(start).map((_, index) => start + index));
-          return acc;
+        if (end >= start && step === 1) {
+          count = end - start + 1;
+        } else if (end >= start && step > 1) {
+          count = end === start ? 1 : Math.ceil((end - start) / step);
+        } else if (end <= start && step < 1) {
+          count = Math.abs(Math.ceil((end - start) / step)) + 1;
         }
 
-        if (end >= start && step > 0) {
-          const count = end === start ? 1 : Math.ceil((end - start) / step);
-          acc = acc.concat(Array(count).fill(start).map((_, index) => start + (index * step)));
-          return acc;
-        }
-
-        if (end <= start && step < 0) {
-          const count = Math.abs(Math.ceil((end - start) / step)) + 1;
-          acc = acc.concat(Array(count).fill(start).map((_, index) => start + (index * step)));
-          return acc;
-        }
-
+        acc = acc.concat(Array(count)
+          .fill(start)
+          .map((_, index) => start + (index * step)));
         return acc;
       }
 
       acc.push(Number.parseInt(str, 10));
       return acc;
     }, []);
-
-  return list;
 };
