c15023e1073ded31a7105e6393bea2ecd9aab3fe
alekseyleshko <alekseyleshko@gmail.com>
2019-02-09 16:18:11 +0300

    Add simple decision for task

---
+++ students-final-grade/test.js
@@ -0,0 +1,14 @@
+import { describe } from 'ava-spec';
+import finalGrade from './solution';
+
+describe('tudents-final-grade:', it => {
+  it('should return answer', t => {
+    t.is(finalGrade(100, 12), 100);
+    t.is(finalGrade(85, 5), 90);
+    t.is(finalGrade(10, 15), 100);
+    t.is(finalGrade(85, 5), 90);
+    t.is(finalGrade(55, 3), 75);
+    t.is(finalGrade(55, 0), 0);
+    t.is(finalGrade(20, 2), 0);
+  });
+});
