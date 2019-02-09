d0c600c269aa9de8f810c0aff46f62a64d74e04a
alekseyleshko <alekseyleshko@gmail.com>
2019-02-09 16:55:17 +0300

    Finish task

--- students-final-grade/solution.js
+++ students-final-grade/solution.js
@@ -1,15 +1,14 @@
-export default (exam, projects) => {
-  if (exam > 90 || projects > 10) {
-    return 100;
-  }
+const excellent = (exam, projects) => exam > 90 || projects > 10;
+const good = (exam, projects) => exam > 75 && projects >= 5;
+const satisfactorily = (exam, projects) => exam > 50 && projects >= 2;
 
-  if (exam > 75 && projects >= 5) {
-    return 90;
-  }
+const createGrade = (predicate, grade) =>
+  (exam, projects) => predicate(exam, projects) ? grade : 0;
 
-  if (exam > 50 && projects >= 2) {
-    return 75;
-  }
-
-  return 0;
-};
+export default (exam, projects) => [
+  createGrade(excellent, 100),
+  createGrade(good, 90),
+  createGrade(satisfactorily, 75)]
+  .map(predicat => predicat(exam, projects))
+  .sort((a, b) => b - a)
+  .shift();
