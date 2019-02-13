47a2820490b9c61142d27ec1412d98ba13f6dc1a
alekseyleshko <alekseyleshko@gmail.com>
2019-02-13 22:06:40 +0300

    Update test

--- custom-sort-function/test.js
+++ custom-sort-function/test.js
@@ -2,9 +2,10 @@ import bubbleSort from './bubble'
 import { describe } from 'ava-spec'
 
 describe('custom-sort-function:', it => {
-  it('should return answer', t => {
+  it('bubble sort', t => {
     t.deepEqual(bubbleSort([1]), [1])
     t.deepEqual(bubbleSort([1, 3, 2]), [1, 2, 3])
     t.deepEqual(bubbleSort(['abc', 'adc', 'acc']), ['abc','acc','adc'])
+    t.deepEqual(bubbleSort([1, 3, 2, -99, 0, 34, 13]), [-99, 0, 1, 2, 3, 13, 34])
   })
 })
