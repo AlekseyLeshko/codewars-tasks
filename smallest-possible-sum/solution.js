8a9c002f1fdfb74f5ca316fe08ecbf34ff85859e
alekseyleshko <alekseyleshko@gmail.com>
2019-02-16 17:30:33 +0300

    Rework decision

--- smallest-possible-sum/solution.js
+++ smallest-possible-sum/solution.js
@@ -1,39 +1,19 @@
-import qsort from './qsort.js'
-
-function difference(max, min) {
-  max = max.split('')
-    .reverse()
-  min = min.split('')
-    .reverse()
-  var len = max.length,
-    result = []
-  for (var i = 0, b = 0, c = 0; i < len; i++) {
-    b = max[i] - (min[i] || 0) + c
-    result[i] = b < 0 ? (c = -1, 10 + b) : (c = 0, b)
+const getMinVal = (n) => {
+  const [i, j] = n.sort((a, b) => a - b)
+  if (isNaN(j % i)) {
+    return j
   }
 
-  return result.reverse()
-    .join('')
-    .replace(/^0+/, '')
+  return getMinVal([j - i, i])
 }
 
-const smallestPossibleSum = (n) => {
-  n = qsort(n)
+const solution = (n) => {
+  if (n.length === 1) return n[0]
 
-  let isOver = true
-  const i = 0
-  for (let j = 1; j < n.length; j++) {
-    if (n[i] > n[j]) {
-      n[i] = +difference(n[i].toString(), n[j].toString())
-      isOver = false
-    }
-  }
-
-  if (isOver) {
-    return n.reduce((sum, item) => sum += item, 0)
-  }
-
-  return smallestPossibleSum(n)
+  const minVal = getMinVal(n)
+  return (new Array(n.length))
+    .fill(minVal)
+    .reduce((sum, item) => sum += item, 0)
 }
 
-export default smallestPossibleSum
+export default solution
