812de5a81b052b8cf6dde9a640ed08239f8bcea0
alekseyleshko <alekseyleshko@gmail.com>
2019-02-16 19:27:31 +0300

    Finish task

--- smallest-possible-sum/solution.js
+++ smallest-possible-sum/solution.js
@@ -1,19 +1,2 @@
-const getMinVal = (n) => {
-  const [i, j] = n.sort((a, b) => a - b)
-  if (isNaN(j % i)) {
-    return j
-  }
-
-  return getMinVal([j - i, i])
-}
-
-const solution = (n) => {
-  if (n.length === 1) return n[0]
-
-  const minVal = getMinVal(n)
-  return (new Array(n.length))
-    .fill(minVal)
-    .reduce((sum, item) => sum += item, 0)
-}
-
-export default solution
+const gcd = (a, b) => a ? gcd(b % a, a) : b
+export default (arr) => arr.reduce(gcd) * arr.length
