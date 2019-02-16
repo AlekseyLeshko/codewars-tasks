91036225de1c96175fffcc6635f8ffb64a4e4f2a
alekseyleshko <alekseyleshko@gmail.com>
2019-02-16 16:54:21 +0300

    Decision with long arithmetic

--- smallest-possible-sum/solution.js
+++ smallest-possible-sum/solution.js
@@ -1,17 +1,39 @@
 import qsort from './qsort.js'
 
-const smallestPossibleSum = (n) => {
-  n = qsort(n)
-  const [i, j] = [...new Set(n)]
-    .slice(0, 2)
-    .map(item => n.indexOf(item))
-
-  if (n[i] > n[j]) {
-    n[i] = n[i] - n[j]
-    return smallestPossibleSum(n)
+function difference(max, min) {
+  max = max.split('')
+    .reverse()
+  min = min.split('')
+    .reverse()
+  var len = max.length,
+    result = []
+  for (var i = 0, b = 0, c = 0; i < len; i++) {
+    b = max[i] - (min[i] || 0) + c
+    result[i] = b < 0 ? (c = -1, 10 + b) : (c = 0, b)
   }
 
-  return n.reduce((sum, item) => sum += item, 0)
+  return result.reverse()
+    .join('')
+    .replace(/^0+/, '')
+}
+
+const smallestPossibleSum = (n) => {
+  n = qsort(n)
+
+  let isOver = true
+  const i = 0
+  for (let j = 1; j < n.length; j++) {
+    if (n[i] > n[j]) {
+      n[i] = +difference(n[i].toString(), n[j].toString())
+      isOver = false
+    }
+  }
+
+  if (isOver) {
+    return n.reduce((sum, item) => sum += item, 0)
+  }
+
+  return smallestPossibleSum(n)
 }
 
 export default smallestPossibleSum
