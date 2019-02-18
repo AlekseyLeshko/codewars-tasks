d07d78d024ead6754a1b10c77537b388a885c7dc
alekseyleshko <alekseyleshko@gmail.com>
2019-02-19 00:07:32 +0300

    Rework solution

--- persistent-bugger/solution.js
+++ persistent-bugger/solution.js
@@ -1,11 +1,4 @@
-const persistence = (num) => {
-  if (Math.floor(num / 10) === 0) return 0
-
-  return 1 + persistence(num
-    .toString()
-    .split('')
-    .map(Number)
-    .reduce((acc, item) => acc *= item, 1))
-}
+const persistence = (num) =>
+  `${num}`.length === 1 ? 0 : 1 + persistence(`${num}`.split('').map(Number).reduce((acc, item) => acc *= item, 1))
 
 export default persistence
