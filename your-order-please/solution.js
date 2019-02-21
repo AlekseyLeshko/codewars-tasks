eb2e6b899a0c8b47ec6749f61c29fa13f75148ac
alekseyleshko <alekseyleshko@gmail.com>
2019-02-21 23:18:09 +0300

    Finish task

--- your-order-please/solution.js
+++ your-order-please/solution.js
@@ -1,6 +1,4 @@
-export default (words) => words.length === 0 ? words : words
+export default (words) => words
   .split(' ')
-  .map(word => ({ word, num: Number(/\d/.exec(word)[0]) }))
-  .sort((a, b) => a.num - b.num)
-  .map(item => item.word)
+  .sort((a, b) => a.match(/\d/) - b.match(/\d/))
   .join(' ')
