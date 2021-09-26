74b1c2bef2de6fcee52b6fbe8b786d2ca2469195
AlekseyLeshko <alekseyleshko@gmail.com>
2021-09-26 17:01:30 +0800

    Use the each feature

--- your-order-please/test.js
+++ your-order-please/test.js
@@ -1,9 +1,16 @@
 import order from './solution'
 
 describe('your order, please:', () => {
-  it('should return answer', () => {
-    expect(order('is2 Thi1s T4est 3a')).toBe('Thi1s is2 3a T4est')
-    expect(order('4of Fo1r pe6ople g3ood th5e the2')).toBe('Fo1r the2 g3ood 4of th5e pe6ople')
-    expect(order('')).toBe('')
+  it.each([{
+    string: 'is2 Thi1s T4est 3a',
+    expected: 'Thi1s is2 3a T4est',
+  }, {
+    string: '4of Fo1r pe6ople g3ood th5e the2',
+    expected: 'Fo1r the2 g3ood 4of th5e pe6ople',
+  }, {
+    string: '',
+    expected: '',
+  }])('should return answer', ({ string, expected }) => {
+    expect(order(string)).toBe(expected)
   })
 })
