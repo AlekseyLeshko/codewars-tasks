a1166155a9365b84c00b68c138ccbb0266dcd09f
AlekseyLeshko <alekseyleshko@gmail.com>
2021-09-26 16:51:00 +0800

    Replace the Ava to the Jest

--- your-order-please/test.js
+++ your-order-please/test.js
@@ -1,10 +1,9 @@
-import { describe } from 'ava-spec'
 import order from './solution'
 
-describe('your order, please:', it => {
-  it('should return answer', t => {
-    t.is(order('is2 Thi1s T4est 3a'), 'Thi1s is2 3a T4est')
-    t.is(order('4of Fo1r pe6ople g3ood th5e the2'), 'Fo1r the2 g3ood 4of th5e pe6ople')
-    t.is(order(''), '')
+describe('your order, please:', () => {
+  it('should return answer', () => {
+    expect(order('is2 Thi1s T4est 3a')).toBe('Thi1s is2 3a T4est')
+    expect(order('4of Fo1r pe6ople g3ood th5e the2')).toBe('Fo1r the2 g3ood 4of th5e pe6ople')
+    expect(order('')).toBe('')
   })
 })
